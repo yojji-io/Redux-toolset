a4d08eb1a4c3f5f68a48e1a29e45283b3802ba6d
Kirill Lykho <kirillliho@yojji.io>
2019-09-20 15:42:21 +0300

    [fix] Removed dead code

--- src/create-simple-reducer/create-simple-reducer.ts
+++ src/create-simple-reducer/create-simple-reducer.ts
@@ -2,40 +2,6 @@ import { Reducer } from 'redux';
 
 import { IAction, IActionObject } from '../action-creator';
 
-// type TCreateSimpleReducer = <State = unknown, Meta = unknown>(
-//   actionToHandle: IAction | string,
-//   reducer: Reducer<
-//     State,
-//     IActionObject<
-//       typeof actionToHandle extends string
-//         ? unknown
-//         : ReturnType<typeof actionToHandle>,
-//       Meta
-//     >
-//   >,
-//   initialState: State
-// ) => (state: State, action: IActionObject<any, Meta>) => State;
-
-// interface ICreateSimpleReducer {
-//   <State = unknown, Meta = unknown>(
-//     actionToHandle: string,
-//     reducer: Reducer<State, IActionObject<unknown, Meta>>,
-//     initialState: State
-//   ): (state: State, action: IActionObject<unknown, Meta>) => State;
-//
-//   <State = unknown, Meta = unknown>(
-//     actionToHandle: IAction,
-//     reducer: Reducer<
-//       State,
-//       IActionObject<ReturnType<typeof actionToHandle>, Meta>
-//     >,
-//     initialState: State
-//   ): (
-//     state: State,
-//     action: IActionObject<ReturnType<typeof actionToHandle>, Meta>
-//   ) => State;
-// }
-
 export function createSimpleReducer<
   State = unknown,
   Payload = unknown,
