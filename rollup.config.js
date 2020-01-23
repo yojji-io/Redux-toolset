119ad5fcfc5ddb4a8bde266b84af757487692321
Kirill Lykho <lykho.kirill@gmail.com>
2020-01-23 13:00:40 +0200

    [feature] Added babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,43 @@ import commonJs from 'rollup-plugin-commonjs';
 import typeScript from 'rollup-plugin-typescript2';
 import visualizer from 'rollup-plugin-visualizer';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
-import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
 
 export default [
   {
     input: 'src/index.ts',
-    output: [{ file: 'build/index.r.min.js', format: 'esm' }],
+    output: [{
+      file: 'build/index.js',
+      format: 'cjs',
+      sourcemap: 'inline',
+      name: '@yojji/core'
+    }],
     plugins: [
       nodeResolve(),
       commonJs(),
       typeScript({ clean: true }),
       sizeSnapshot(),
-      terser(),
       visualizer(),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+              spec: true,
+              forceAllTransforms: true,
+              useBuiltIns: 'usage',
+              corejs: 3,
+              polyfills: ['es7.object.entries, es7.object.values'],
+              targets: {
+                node: 8,
+                ie: 11
+              },
+            }
+          ]
+        ]
+      })
     ],
   },
 ];
